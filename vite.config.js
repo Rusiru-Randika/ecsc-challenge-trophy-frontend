abe4693c9e6cdb042ba306fe3c6983491a86913f
Rusiru Randika <rusirurandikakmv@gmail.com>
2025-11-09 11:34:34 +0530

    fix: Update Vite configuration to load backend URL from environment variable

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(() => {
-  const BACKEND_URL = 'https://ecsc-challenge-trophy-backend.onrender.com'
-
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
+  
   return {
     plugins: [react()],
     server: {
       port: 3000,
       proxy: {
         '/api': {
-          target: BACKEND_URL,
+          target: env.VITE_API_URL || 'http://localhost:5000',
           changeOrigin: true
         }
       }
